041dab068355c30bcab028713f0e446e1001645b
makelannika <makelannika@gmail.com>
2024-07-10 16:51:30 +0300

    finished

--- philo/src/arg_validation.c
+++ philo/src/arg_validation.c
@@ -6,45 +6,37 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 19:27:32 by amakela           #+#    #+#             */
-/*   Updated: 2024/07/06 20:56:26 by amakela          ###   ########.fr       */
+/*   Updated: 2024/07/09 19:38:04 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
-
-static int	overflow(int neg)
-{
-	if (neg == -1)
-		return (0);
-	return (-1);
-}
+#include <limits.h>
 
 int	ft_atoi(const char *str)
 {
 	int		i;
 	long	nbr;
-	int		neg;
-	long	prev;
+	int		sign;
 
 	i = 0;
 	nbr = 0;
-	neg = 1;
+	sign = 1;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
+	if (str[i] == '-')
+		sign = -1;
 	if ((str[i] == '-') || (str[i] == '+'))
-	{
-		if (str[i] == '-')
-			neg = -1;
 		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i])
 	{
-		prev = nbr;
+		if (str[i] < '0' || str[i] > '9')
+			return (-1);
 		nbr = 10 * nbr + str[i++] - '0';
-		if (prev > nbr)
-			return (overflow(neg));
+		if (nbr > INT_MAX)
+			return (-1);
 	}
-	return ((int)nbr * neg);
+	return ((int)nbr * sign);
 }
 
 static int	check_values(char **argv)
@@ -64,25 +56,7 @@ static int	check_values(char **argv)
 
 int	arg_validation(int argc, char **argv)
 {
-	int	i;
-	int	j;
-
-	i = 1;
-	j = 0;
 	if (argc < 5 || argc > 6)
 		return (write(2, "error: invalid amount of arguments\n", 35));
-	while (argv[i])
-	{
-		if (argv[i][j] == '+' || argv[i][j] == '-')
-			j++;
-		while (argv[i][j])
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-				return (write(2, "error: arguments must be numbers only\n", 38));
-			j++;
-		}
-		i++;
-		j = 0;
-	}
 	return (check_values(argv));
 }
